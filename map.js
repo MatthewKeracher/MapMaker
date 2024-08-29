e06d9935408bcbb8c0b12f80b577778b4359c7ef
Matthew Keracher <76267232+MatthewKeracher@users.noreply.github.com>
2024-08-29 15:30:24 +0200

    Removed CORS
    Have removed CORS prefix.

--- map.js
+++ map.js
@@ -27,7 +27,7 @@ let imageBlob;
 if(url){
 console.log('Receievd URL', url)
 // Fetch the image from the URL if provided
-const response = await fetch('https://corsproxy.io/?' + url);
+const response = await fetch(url);
 imageBlob = await response.blob();
 
 }else{
